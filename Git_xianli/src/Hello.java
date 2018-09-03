91a0d4b0c5aabd6c2853e45f748e8c2a6760d763
xuquanya <1358219131@qq.com>
2018-09-03 17:14:59 +0800

    修改后提交

--- Git_xianli/src/Hello.java
+++ Git_xianli/src/Hello.java
@@ -2,4 +2,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("nihao");
     }
+
+    public void test(){
+        int a=10;
+        a +=20;
+    }
 }
